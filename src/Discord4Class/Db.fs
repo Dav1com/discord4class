e0525e9c77f9fcb8a7b0ac58948614b675b3cf8e
Dav1com <david@ibanezd.ml>
2020-09-23 11:28:44 -0300

    Wrong module name

--- src/Discord4Class/Db.fs
+++ src/Discord4Class/Db.fs
@@ -3,19 +3,18 @@ namespace Discord4Class
 open System
 open MongoDB.Driver
 open MongoDB.Bson
-open Discord4Class.DbDriver.Types
 
-module Bd =
+module Db =
 
     [<Literal>]
     let DbName = "Discord4Class"
 
-    let openConnection url user pass db =
+    let openConnection url user pass =
         let connectionString =
             sprintf "mongodb://%s%s%s"
                 (if not (String.IsNullOrEmpty user) then user + ":" else "" )
                 (if not (String.IsNullOrEmpty pass) then pass else "" )
                 (if not (String.IsNullOrEmpty user) then "@url" else url)
 
-        MongoClient(connectionString).GetDatabase db
+        MongoClient(connectionString).GetDatabase DbName
 
