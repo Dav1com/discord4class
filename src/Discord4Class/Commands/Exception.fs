9df6a795c883f52da6a5ba86194281876090b648
Dav1com <david@ibanezd.ml>
2020-09-24 14:15:22 -0300

    moved embed creation to helper

--- src/Discord4Class/Commands/Exception.fs
+++ src/Discord4Class/Commands/Exception.fs
@@ -2,12 +2,10 @@ namespace Discord4Class.Commands
 
 open System
 open System.Threading.Tasks
-open DSharpPlus
 open DSharpPlus.Entities
-open DSharpPlus.CommandsNext
-open DSharpPlus.CommandsNext.Exceptions
 open DSharpPlus.EventArgs
 open Discord4Class.Helpers.Permission
+open Discord4Class.Helpers.Embed
 open Discord4Class.Config.Types
 
 module Exception =
@@ -18,24 +16,16 @@ module Exception =
     [<Literal>]
     let private EmbedColor = 0xFF0000
 
-    let private guildErrorEmbed config (client : DiscordUser) message =
-        DiscordEmbedBuilder()
-            .WithColor( DiscordColor(EmbedColor) )
-            .WithAuthor(config.Guild.Lang.ErrorEmbedAuthor)
-            .WithFooter(config.Bot.BotName, client.AvatarUrl)
-            .WithTimestamp( Nullable<DateTimeOffset>(DateTimeOffset.Now) )
-            .WithDescription(message)
-            .Build()
-
-    let cmdNotFound config (msg : MessageCreateEventArgs) (e : CommandNotFoundException) =
-        printfn "CmdException on command: %s" e.Command
+    let cmdNotFound cmd config (msg : MessageCreateEventArgs) =
         let embed =
-            config.Guild.Lang.ErrorCmdNotFound msg.Author.Mention msg.Message.Content
-            |> guildErrorEmbed config msg.Client.CurrentUser
-        msg.Channel.SendMessageAsync("", false, embed) :> Task
+            config.Guild.Lang.ErrorCmdNotFound cmd
+            |> newEmbed EmbedColor config.Guild.Lang.ErrorEmbedAuthor
+        msg.Channel.SendMessageAsync(msg.Author.Mention, false, embed)
+        :> Task
 
     let cmdErrorUnknown config (msg : MessageCreateEventArgs) (ex : exn) =
         let embed =
-            config.Guild.Lang.ErrorCmdUnknown msg.Author.Mention (ex.GetType().ToString()) ex.Message
-            |> guildErrorEmbed config msg.Client.CurrentUser
-        msg.Channel.SendMessageAsync("", false, embed) :> Task
+            config.Guild.Lang.ErrorCmdUnknown (ex.GetType().ToString()) ex.Message
+            |> newEmbed EmbedColor config.Guild.Lang.ErrorEmbedAuthor
+        msg.Channel.SendMessageAsync(msg.Author.Mention, false, embed)
+        :> Task
