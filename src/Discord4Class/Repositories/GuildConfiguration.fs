e16e1de70e3c177e8efaefae208407c4e02f5e71
Dav1com <david@ibanezd.ml>
2020-09-24 14:00:53 -0300

    Created GuildConfiguration repository

--- src/Discord4Class/Repositories/GuildConfiguration.fs
+++ src/Discord4Class/Repositories/GuildConfiguration.fs
@@ -10,18 +10,43 @@ module GuildConfiguration =
     let private CollectionName = "GuildConfiguration"
 
     type GuildConfiguration =
-      { GuildId : uint64
-        CommandPrefix : String option
-        Language : String option
-        ClassTextChannel : uint64
-        ClassVoiceChannel : uint64
-        TeachersTextChannel : uint64
-        TeachersVoiceChannel : uint64
-        AdminRole : uint64
-        TeacherRole : uint64
-        StudentRole : uint64
-        GroupPersistence : bool
-        IsEveryoneStudent : bool }
+      { _id : uint64 //GuildId
+        CommandPrefix : string
+        Language : string }
 
-        static member Get (db : IMongoDatabase) (filter : FilterDefinitionBuilder<GuildConfiguration>) =
-            ()
+        static member Filter = Builders<GuildConfiguration>.Filter
+        static member Update = Builders<GuildConfiguration>.Update
+
+        static member GetOne (db : IMongoDatabase) (filter : FilterDefinition<GuildConfiguration>) =
+            let result =
+                db.GetCollection<GuildConfiguration>(CollectionName)
+                    .Find<GuildConfiguration>(filter)
+            if result.CountDocuments() = 0L then
+                async {return None}
+            else
+                async {return Some <| result.First()}
+
+        static member GetMany (db : IMongoDatabase) (filter : FilterDefinition<GuildConfiguration>) = async {
+            return
+                db.GetCollection<GuildConfiguration>(CollectionName)
+                    .Find<GuildConfiguration>(filter).ToList()
+                |> List.ofSeq
+        }
+
+        static member Insert (db : IMongoDatabase) (obj : GuildConfiguration) =
+            db.GetCollection<GuildConfiguration>(CollectionName)
+                .InsertOneAsync(obj)
+            |> Async.AwaitTask
+
+        static member InsertMany (db : IMongoDatabase) (ls : GuildConfiguration list) =
+            db.GetCollection<GuildConfiguration>(CollectionName)
+                .InsertManyAsync(ls)
+            |> Async.AwaitTask
+
+        static member UpdateOne (db : IMongoDatabase) (filter : FilterDefinition<GuildConfiguration>) (update : UpdateDefinition<GuildConfiguration>) =
+            db.GetCollection<GuildConfiguration>(CollectionName)
+                .UpdateOneAsync(filter, update)
+            |> Async.AwaitTask
+
+    type GC = GuildConfiguration
+
