e5fe8ca645dfbec894d60d90d5d20f39bea2daa0
Dav1com <david@ibanezd.ml>
2020-09-26 22:36:11 -0300

    Now loading lang files will fail if an error happend

--- src/Discord4Class/Lang/Loader.fs
+++ src/Discord4Class/Lang/Loader.fs
@@ -7,13 +7,13 @@ open Discord4Class.Lang.Types
 
 module Loader =
 
-    let private loadLangFileAsync lang (fullPath : string) = async {
+    let private loadLangFileAsync (lang : string) (fullPath : string) = async {
         let c =
             ConfigurationBuilder()
                 .AddIniFile(fullPath)
                 .Build()
         let ac = AppConfig c
-        return (lang, ac.Get<LangStrings> ())
+        return (lang.ToLower(), ac.Get<LangStrings> ())
     }
 
     let loadLangFiles path =
@@ -23,14 +23,11 @@ module Loader =
         )
         |> Async.Parallel
         |> Async.RunSynchronously
-        |> Array.filter (fun (_,r) ->
-            match r with
-            | Ok _ -> true
-            | Error _ -> false
-        )
         // TODO: remove the warning
-        |> Array.map (fun (s, Ok l) ->
-            (s, LangBuilders.OfStrings (l.SanitizeBackslashes()) )
+        |> Array.map (fun (s, la) ->
+            match la with
+            | Ok l -> (s, LangBuilders.OfStrings (l.SanitizeBackslashes()) )
+            | Error l -> failwithf "Error while loading lang file '%s': %A" s l
         )
         |> Map.ofSeq
 
