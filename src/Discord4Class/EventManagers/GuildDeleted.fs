14b231ca8413ade1eb26d9fd43e51c6bd392e356
Dav1com <david@ibanezd.ml>
2020-09-26 22:46:01 -0300

    Added GuildDeleted handler, for db cleanup

---
+++ src/Discord4Class/EventManagers/GuildDeleted.fs
@@ -0,0 +1,21 @@
+namespace Discord4Class.EventManagers
+
+open System.Threading.Tasks
+open MongoDB
+open DSharpPlus.EventArgs
+open Discord4Class.Config.Types
+open Discord4Class.Repositories.GuildConfiguration
+
+module GuildDeleted =
+
+    let exec config (e : GuildDeleteEventArgs) =
+        async {
+            [
+                GC.DeleteOne config.App.DbDatabase (GC.Filter.And [
+                    GC.Filter.Eq((fun g -> g._id), e.Guild.Id)
+                ])
+            ]
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> ignore
+        } |> Async.StartAsTask :> Tasl
