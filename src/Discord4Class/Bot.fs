b0a38f506539c11f34889676c2777002f7f81dbe
Dav1com <david@ibanezd.ml>
2020-09-26 22:34:06 -0300

    Added interactivity module

--- src/Discord4Class/Bot.fs
+++ src/Discord4Class/Bot.fs
@@ -4,6 +4,7 @@ open System
 open System.Threading.Tasks
 open DSharpPlus
 open DSharpPlus.EventArgs
+open DSharpPlus.Interactivity
 open Discord4Class.Config
 open Discord4Class.Config.Types
 open Discord4Class.EventManagers
@@ -37,15 +38,24 @@ module Bot =
         let discord = new DiscordClient( getDiscordConfig config )
         discord.DebugLogger.LogMessageReceived.AddHandler(EventHandler<DebugLogMessageEventArgs>(Log))
 
+        let iconf = InteractivityConfiguration()
+        iconf.Timeout <- TimeSpan.FromSeconds 10.0
+        iconf.PaginationBehaviour <- TimeoutBehaviour.Default
+        iconf.PaginationTimeout <- TimeSpan.FromMinutes 5.0
+
+        let interactivity = discord.UseInteractivity(iconf)
+
+        // Commands
         AsyncEventHandler<MessageCreateEventArgs>(MessageCreated.exec config)
         |> discord.add_MessageCreated
-        //TODO: discord.add_GuildCreated // send welcome message
+        // Clean database
+        //AsyncEventHandler<GuildDeleteEventArgs>(GuildDeleted.exec config)
+        //|> discord.add_GuildDeleted
         //TODO: discord.add_GuildRoleCreated //asks for quick actions
         //TODO: discord.add_GuildRoleDeleted //check roles integrity and updates
-        //TODO: discord.add_GuildRoleUpdated //check roles integrity and updates
         //TODO: discord.add_ChannelDeleted //check channels integrity and updates
-        //TODO: discord.add_ChannelUpdated //check channels integrity and updates
-        //TODO: discord.add_VoiceStateUpdated //assitence service
+        //TODO: discord.add_VoiceStateUpdated //assistence service
+        //TODO: discord.add_MessageReactionAdded // Teachers mark as solved questions
 
         do! discord.ConnectAsync() |> Async.AwaitTask
         Console.add_CancelKeyPress (new ConsoleCancelEventHandler (closeBot discord) )
