d1fbe2d4f182cbfaffcc8f4c38606dac11adb054
Dav1com <david@ibanezd.ml>
2020-10-18 14:13:17 -0300

    Created constants file

---
+++ src/Discord4Class/Constants.fs
@@ -0,0 +1,11 @@
+namespace Discord4Class
+
+module Constants =
+    [<Literal>]
+    let AppName = "Discord For Classrooms"
+    [<Literal>]
+    let AppVersion = "0.0.0"
+    [<Literal>]
+    let IniPath = "config.ini"
+    [<Literal>]
+    let MessageMaxLength = 2000
