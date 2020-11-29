257786731bf05d45b8e1bf5e1924c79af116be2e
Dav1com <david@ibanezd.ml>
2020-11-29 14:53:15 -0300

    Added string helper

--- src/Discord4Class/Helpers/String.fs
+++ src/Discord4Class/Helpers/String.fs
@@ -1,9 +1,26 @@
 namespace Discord4Class.Helpers
 
+open System
+
 module String =
 
     let isNumeric =
-        String.forall (fun c ->
-            let diff = (int) c - 48
-            0 <= diff && diff < 10
-        )
+        String.forall (fun c -> '0' <= c && c <= '9')
+
+    let isSnakeCase =
+        String.forall (fun c -> ('a' <= c && c <= 'z') || c = '_')
+
+    let (|IntParse|_|) (s: string) =
+        match Int32.TryParse s with
+        | (false, _) -> None
+        | (true, i) -> Some i
+
+    let (|Int64Parse|_|) (s: string) =
+        match Int64.TryParse s with
+        | (false, _) -> None
+        | (true, i) -> Some i
+
+    let (|UInt64Parse|_|) (s: string) =
+        match UInt64.TryParse s with
+        | (false, _) -> None
+        | (true, i) -> Some i
