0a935f8325205df1918029363eed6c23a61a350a
Dav1com <david@ibanezd.ml>
2020-11-29 14:48:58 -0300

    Added exceptions

---
+++ src/Discord4Class/Exceptions.fs
@@ -0,0 +1,10 @@
+namespace Discord4Class
+
+module Exceptions =
+
+    // code level exceptions
+    exception InsertUpdateNothingException
+    exception CommandConfigInvalidFieldException
+    exception TeamsMoveReturnUnexpectedErrorException
+    exception BadArgumentPhrasingException
+    exception BadCommandConfigException
