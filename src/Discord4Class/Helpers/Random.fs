cd860a50eba099d220bd92bb69a52e61e19f15a4
Dav1com <david@ibanezd.ml>
2020-11-29 14:53:42 -0300

    Added random helper

--- src/Discord4Class/Helpers/Random.fs
+++ src/Discord4Class/Helpers/Random.fs
@@ -4,14 +4,12 @@ open System
 
 module Random =
 
-    let randomIndexes hi =
+    let randomPermute arr =
         let rand = Random()
-        let output = [|0..hi-1|]
-        output
+        arr
         |> Array.iteri (fun i _ ->
-            let rnd = rand.Next(0, hi);
-            let tmp = output.[i]
-            output.[i] <- output.[rnd]
-            output.[rnd] <- tmp
-        )
-        output
+            let rnd = rand.Next(0, arr.Length)
+            let tmp = arr.[i]
+            arr.[i] <- arr.[rnd]
+            arr.[rnd] <- tmp )
+        arr
