0d516d6ce38a153b22cf9e179610162343b0c092
Dav1com <david@ibanezd.ml>
2020-11-29 15:47:52 -0300

    Added fields

--- src/Discord4Class/Config/Types.fs
+++ src/Discord4Class/Config/Types.fs
@@ -1,5 +1,6 @@
 namespace Discord4Class.Config
 
+open DSharpPlus.Entities
 open MongoDB.Driver
 open Discord4Class.Lang.Types
 open Discord4Class.Config.InnerTypes
@@ -7,22 +8,26 @@ open Discord4Class.Config.InnerTypes
 module Types =
 
     type AppConfig =
-      { DocsURL : string
-        JoinGuildURL : string
-        Emojis : Emoji
-        AllLangs : Map<string, LangBuilders>
-        Db : IMongoDatabase }
+        { DocsURL: string
+          JoinGuildURL: string
+          SupportServer: string
+          Emojis: Emojis
+          AllLangs: Map<string, LangBuilders>
+          Db: IMongoDatabase
+          CommandByMention: bool }
 
     type GuildConfig =
-      { Lang : LangBuilders
-        CommandPrefix : string
-        IsConfigOnDb : bool
-        TeachersText : uint64 option
-        ClassVoice : uint64 option
-        TeacherRole : uint64 option }
+        { Lang: LangBuilders
+          LangName: string
+          CommandPrefix: string
+          IsConfigOnDb: bool
+          TeachersText: uint64 option
+          ClassVoice: uint64 option
+          TeacherRole: uint64 option
+          DoingHeavyTask: bool }
 
     type Config =
-      { Bot : BotConf
-        Persistence : Persistence
-        Preferences : Preferences
-        App : AppConfig }
+        { Bot: BotConf
+          Persistence: Persistence
+          Preferences: Preferences
+          App: AppConfig }
