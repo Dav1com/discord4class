7489df58cd4621fddacf9cae5587d1bb3542abd1
Dav1com <david@ibanezd.ml>
2020-11-29 15:09:13 -0300

    Added constants

--- src/Discord4Class/Constants.fs
+++ src/Discord4Class/Constants.fs
@@ -1,5 +1,7 @@
 namespace Discord4Class
 
+open DSharpPlus
+
 module Constants =
     [<Literal>]
     let AppName = "Discord For Classrooms"
@@ -8,4 +10,22 @@ module Constants =
     [<Literal>]
     let IniPath = "config.ini"
     [<Literal>]
+    let LangPath = "res/lang/"
+    [<Literal>]
+    let LargeGuildThreshold = 1000 // move to config.ini ¿?
+    [<Literal>]
     let MessageMaxLength = 2000
+    [<Literal>]
+    let EmbedDescriptionMaxLength = 2048
+    [<Literal>]
+    let GuildMaxChannels = 500
+    [<Literal>]
+    let GuildMaxRoles = 250
+    [<Literal>]
+    let PrefixMaxSize = 2
+    [<Literal>]
+    let TeamsChannelsFactor = 3 // channels per teams
+    [<Literal>]
+    let DiscordDomain = "discord.com/"
+    [<Literal>]
+    let GuildPrivilegedPerm = Permissions.ManageGuild
